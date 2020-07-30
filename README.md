17aa31dfdb083020d508974d7dc3c8cd0118faed
akio <akiozihaoceat@gmail.com>
2020-07-31 02:26:18 +0800

    0.0.2 add rat_dataset train multibox_loss README

---
+++ README.md
@@ -0,0 +1,5 @@
+Torch1.4.0、CUDA10.1、cudnn7.5.1、OpenCV4.1.1、OpenVINO2020R1
+
+数据集格式:
+读取三个文件 train val test.txt 包含所有对应样本的路径
+在ratdataset中读取
--- models/ssd/datasets/rat_dataset.py
+++ models/ssd/datasets/rat_dataset.py
@@ -30,16 +30,71 @@ class RATDataset(Dataset):
     def __getitem__(self, index):
         image_id = self.ids[index]
         boxes, labels = self._get_annotation(image_id)
+        image = self._read_image(image_id)
+        if self.transform:
+            image, boxes, labels = self.transform(image, boxes, labels)
+        if self.target_transform:
+            boxes, labels = self.target_transform(boxes, labels)
+        return image, boxes, labels
+
+    def __len__(self):
+        return len(self.ids)
+
+    def get_image(self, index):
+        image_id = self.ids[index]
+        image = self._read_image(image_id)
+        if self.transform:
+            image, _ = self.transform(image)
+        return image
+
+    def get_annotation(self, index):
+        image_id = self.ids[index]
+        return image_id, self._get_annotation(image_id)
 
     def _get_annotation(self, image_id):
         # annotation_file = self.root / f"Annotations/
         try:
-            annotation_file = os.path.join(self.root, f"CHTXrat{image_id}")
+            annotation_file = os.path.join(self.root, f"CHTXrat{image_id}.xml") # TODO CHECK THIS
+            objects = ET.parse(annotation_file).findall("object")
+        except:
+            annotation_file = os.path.join(self.root, f"CHTXrat{image_id}.xml")
+            objects = ET.parse(annotation_file).findall("object")
+            print("load annotation error")
 
-        @staticmethod
-        def _read_image_ids(image_sets_file):
-            ids = []
-            with open(image_sets_file) as f:
-                for line in f:
-                    ids.append(line.rstrip())
-            return ids
+        boxes = []
+        labels = []
+        for object in objects:
+            class_name = object.find('name').text.lower().strip()
+
+            if class_name in self.class_dict:
+                bbox = object.find('bndbox')
+                # VOC dataset format follows Matlab, in which indexes start from 0
+                x1 = float(bbox.find('xmin').text) - 1
+                y1 = float(bbox.find('ymin').text) - 1
+                x2 = float(bbox.find('xmax').text) - 1
+                y2 = float(bbox.find('ymax').text) - 1
+                boxes.append([x1, y1, x2, y2])
+                labels.append(self.class_dict[class_name])
+
+        return (np.array(boxes, dtype=np.float32),
+                np.array(labels, dtype=np.int64))
+
+    def _read_image(self, image_id):
+        try:
+            image_file = "./data/VOCdevkit/VOC2007/" + f"JPEGImages/{image_id}.jpg"
+            image = cv2.imread(str(image_file))
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        except:
+            image_file = "./data/VOCdevkit/test/VOC2007/" + f"JPEGImages/{image_id}.jpg"
+            image = cv2.imread(str(image_file))
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            print("read image error")
+        return image
+
+    @staticmethod
+    def _read_image_ids(image_sets_file):
+        ids = []
+        with open(image_sets_file) as f:
+            for line in f:
+                ids.append(line.rstrip())
+        return ids
