aea2d15e0b0386510e2c55bf84fa7dc5bb6a4bb1
LesterLian <32392052+LesterLian@users.noreply.github.com>
2020-08-01 00:37:40 -0400

    Update config.py
    test ignorance when pull

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ TRAIN_IMAGES_PATH="/home/data/train_images.txt"
 TRAIN_XMLS_PATH = "/home/data/train_xmls.txt"
 VAL_IMAGES_PATH = "/home/data/val_images.txt"
 VAL_XMLS_PATH = "/home/data/val_xmls.txt"
+test = ''
